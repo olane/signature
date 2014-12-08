fb28301b26a7142ab7c0a4241533292cb4c9d083
Oli Lane <ojgl2@cam.ac.uk>
2014-12-08 12:11:51 +0000

    Added basic class structure

---
+++ +algorithms/+constellation/@builder/builder.m
@@ -0,0 +1,14 @@
+classdef builder
+    %BUILDER Builds the fingerprint database for this algorithm
+    %   Takes audio data, creates an audio fingerprint from it, and inserts
+    %   it into a database. These fingerprints can be used later by the
+    %   matcher to match audio clips.
+    
+    properties
+    end
+    
+    methods
+    end
+    
+end
+
