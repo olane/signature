a1930fda91162985a0d5f84e83a0b5000ce844ae
Oli Lane <ojgl2@cam.ac.uk>
2014-12-25 12:56:41 +0000

    Remove redundant testing code

--- +algorithms/+barcoder/testrunner.m
+++ +algorithms/+barcoder/testrunner.m
@@ -4,9 +4,6 @@
 
 filename = 'library/01 - Love Me Again.mp3';
 
-inf = audioinfo(filename);
-sample = [0 100] * inf.SampleRate + 1;
-
 [D, sample_rate] = audioread(filename);
 
 D = mean(D, 2); %stereo to mono
