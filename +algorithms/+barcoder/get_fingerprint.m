5f9a43c9252a4f223ecf13144cff738c88426ca5
Oli Lane <ojgl2@cam.ac.uk>
2014-12-25 12:50:12 +0000

    Finished implementation of barcoder get_fingerprint (not fully tested)

--- +algorithms/+barcoder/get_fingerprint.m
+++ +algorithms/+barcoder/get_fingerprint.m
@@ -1,5 +1,5 @@
 
-function [ hashes ] = get_fingerprint( audio, fs )
+function F = get_fingerprint( audio, fs )
 %GET_FINGERPRINT Creates a fingerprint of a mono audio clip. 
 %   http://www.nhchau.com/files/AudioFingerprint-02-FP04-2.pdf
     
@@ -14,8 +14,9 @@ function [ hashes ] = get_fingerprint( audio, fs )
     
     % We have a real valued signal so half of the fourier transform is
     % redundant
-    fourier_frames = fourier_frames(1:floor(L/2)+1);
+    fourier_frames = fourier_frames(:, 1:floor(L/2)+1);
     
+    n_frames = length(fourier_frames(:, 1));
     
     % Sample length is L, so we have L/2 frequency bins. Our max
     % frequency is fs/2 Hz (by Nyquist), so our bin resolution is
@@ -23,45 +24,57 @@ function [ hashes ] = get_fingerprint( audio, fs )
 
     res = fs/L;
     
-    for frame_num = 1:length(fourier_frames(1))
-        
-        %% Calculate logarithmic bands
-        bottom = 300;
-        top = 2000;
-        n_bands = 33;
+    
+    %% Calculate logarithmic bands
+    % We use n_bands frequency bands between top_freq and bottom_freq,
+    % logarithmically spaced
+    bottom_freq = 300;
+    top_freq = 2000;
+    n_bands = 33;
 
-        % Find logs of endpoints of the line
-        s = log(bottom); 
-        f = log(top);
+    % Find logs of endpoints of the line
+    s = log(bottom_freq); 
+    f = log(top_freq);
 
-        % Then make an evenly spaced vector in log space
-        exponents = linspace(s, f, n_bands+1); 
+    % Then make an evenly spaced vector in log space
+    exponents = linspace(s, f, n_bands+1); 
 
-        % Recover the values in linear space
-        bands = exp(exponents);
-
-
-        %% Sum energy over each band
-        band_energies = zeros(n_bands, 1);
-
-        for i = 1:n_bands
+    % Recover the band limits in linear space
+    bands = exp(exponents);
+    
+    
+    %% Sum energy over each band for each frame
+    
+    % n is frame number, m is band number
+    
+    E = zeros(n_frames, n_bands);
+    
+    for n = 1:n_frames
+        for m = 1:n_bands
             
             % Find first and last bin of the band
-            band = [round(bands(i)/res), round(bands(i+1)/res) - 1];
+            band = [round(bands(m)/res), round(bands(m+1)/res) - 1];
             
             % Calculate the ESD for the band (sum of squares of fourier
             % coeffs)
-            band_energies(i) = sum(fourier_frames(frame_num, band(1):band(2)).^2);
+            E(n,m) = sum(fourier_frames(n, band(1):band(2)).^2);
             
         end
-
-
-
-        %% Calculate F(n,m) using band energies
-    
     end
     
     
-    %% return Fs
+    %% Calculate F(n, m) using band energies
+    
+    % n is frame number, m is bit number
+
+    F = zeros(n_frames-1, n_bands-1);
+
+    for n = 2:n_frames
+        for m = 1:(n_bands - 1)
+
+            F(n-1,m) = (E(n,m) - E(n,m+1) - (E(n-1,m) - E(n-1,m+1))) > 0;
+            
+        end
+    end
     
 end
--- +algorithms/+barcoder/testrunner.m
+++ +algorithms/+barcoder/testrunner.m
@@ -7,11 +7,11 @@ filename = 'library/01 - Love Me Again.mp3';
 inf = audioinfo(filename);
 sample = [0 100] * inf.SampleRate + 1;
 
-[D, sample_rate] = audioread(filename, sample);
+[D, sample_rate] = audioread(filename);
 
 D = mean(D, 2); %stereo to mono
 
-target_sample_rate = 8000;
+target_sample_rate = 5000;
 
 if(sample_rate ~= target_sample_rate)
     %resample to target rate
