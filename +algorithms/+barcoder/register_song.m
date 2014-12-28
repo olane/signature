e84736e15faf0e7d29d4efaa8d3dd0e670a26f17
Oli Lane <ojgl2@cam.ac.uk>
2014-12-28 18:50:13 +0000

    First implementation of register_song

--- +algorithms/+barcoder/register_song.m
+++ +algorithms/+barcoder/register_song.m
@@ -5,12 +5,56 @@ function register_song( db_handle, audio, sample_rate, song_name )
 
     %% Analysis
     
+    tic
     F = algorithms.barcoder.get_fingerprint(audio, sample_rate);
     
-    F_int = rowfun(@utils.bi2dec, F);
+    disp('Analysed song');
+    toc
+    
+    
+    tic
+    F_int = zeros(length(F(:,1)));
+    
+    for i=1:length(F(:,1))
+        F_int(i) = utils.bi2dec(F(i,:));
+    end
+    
+    disp('Packed hashes');
+    toc
+    
     
     %% DB insertion
 
+    sqlite3.execute(db_handle, ...
+                    'INSERT INTO songs VALUES (NULL, ?)', song_name);
+    
+    id = sqlite3.execute(db_handle, ...
+                         'SELECT song_id FROM songs WHERE song_name=?', ...
+                         song_name);
+    
+    id = id(1).song_id;
+    
+    disp(['Inserted song and got song ID ' num2str(id)]);
+    
+    
+    tic
+    sqlite3.execute(db_handle, 'BEGIN TRANSACTION;');
+    
+    for i = 1:length(F_int)
+       sqlite3.execute(db_handle, ...
+                       'INSERT INTO hashes(song_id, hash, hash_number) VALUES (?, ?, ?)', ...
+                       id, F(i), i);
+    end
+    
+    sqlite3.execute(db_handle, 'END TRANSACTION;');
+    
+    disp('Inserted hashes');
+    toc
+    
+    
+    %% Cleanup
+    
+    disp('Song successfully registered.');
     
 
 end
--- +algorithms/+barcoder/testrunner.m
+++ +algorithms/+barcoder/testrunner.m
@@ -1,4 +1,40 @@
 
+addpath('/Users/olane/Documents/MATLAB/matlab-sqlite3-driver/');
+
+database_filename = 'barcoder.db';
+
+
+%% Database initialisation
+
+db_handle = sqlite3.open(database_filename);
+
+exists = sqlite3.execute(db_handle, ...
+                         ['SELECT count(*) FROM sqlite_master '...
+                          'WHERE type=''table'' AND name=''hashes''']);
+
+if(exists.count ~= 1)
+     sqlite3.execute(db_handle, ...
+                     ['CREATE TABLE IF NOT EXISTS '...
+                      'songs (song_id INTEGER PRIMARY KEY AUTOINCREMENT, '...
+                             'song_name VARCHAR)']);
+
+     sqlite3.execute(db_handle, ...
+                     ['CREATE TABLE IF NOT EXISTS '...
+                      'hashes (song_id INTEGER, '...
+                              'hash INTEGER, '...
+                              'hash_number INTEGER)']);
+
+
+     sqlite3.execute(db_handle, ...
+                     'CREATE INDEX songname_index ON songs(song_name)');
+end
+
+ % Drop our hash index to improve speed (we'll re-make it at the
+ % end)
+ sqlite3.execute(db_handle, ...
+                 'DROP INDEX IF EXISTS hash_index');
+
+                 
 %% Fingerprinter 
 
 
@@ -17,4 +53,4 @@ if(sample_rate ~= target_sample_rate)
 end
 
 
-h = algorithms.barcoder.get_fingerprint(D, target_sample_rate);
+algorithms.barcoder.register_song(db_handle, D, target_sample_rate, filename);
