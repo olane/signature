035e43c74266e45fdb0a7e13ecfd9f8bb3d1687a
Oli Lane <ojgl2@cam.ac.uk>
2015-02-13 11:16:53 +0000

    Normalise recorded audio

--- +utils/record_audio.m
+++ +utils/record_audio.m
@@ -1,12 +1,14 @@
 function audiodata = record_audio( sample_rate, bits_per_sample, length )
 %RECORD_AUDIO records some audio from the microphone and returns it as an
-%array. Length given in seconds. Blocks for the length of the recording.
+%array, normalised between -1 and 1. Length given in seconds. Blocks for
+%the length of the recording. 
 
     recObj = audiorecorder(sample_rate, bits_per_sample, 1);
-    
     recordblocking(recObj, length);
-    
     audiodata = getaudiodata(recObj);
+    
+    % Normalise, with 1% headroom
+    audiodata = audiodata ./ max(audiodata) .* 0.99;
     
 end
 
